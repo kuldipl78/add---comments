aa3fe6c5295564f2cf06c2af3ab75b315c30f458
kuldip lohare <162694586+kuldipl78@users.noreply.github.com>
2024-04-03 16:07:13 +0530

    Update index.js

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,10 +1,11 @@
 import './index.css'
+
 const CommentItem = props => {
-  const {comment, onToggeleLike, onDelete} = props
-  const {name, text, liked, id} = comment
+  const {comment, onToggleLike, onDelete} = props // Corrected typo here
+  const {name, text, liked, id, date, initialClassName} = comment
 
   const onToggle = () => {
-    onToggeleLike(id)
+    onToggleLike(id) // Corrected typo here
   }
 
   const onDeleteComment = () => {
@@ -12,33 +13,49 @@ const CommentItem = props => {
   }
 
   return (
-    <li className="list-Container">
-      <div className="comment-Section">
+    <ul className="list-Container">
+      <li className="comment-Section">
         <p className="name">
-          <span className="first-letter">{name[0]}</span> {name}{' '}
+          <span className={initialClassName}>{name[0]}</span> {name}
         </p>
-        <p>{id}</p>
-      </div>
+        <p>{date}</p>
+      </li>
       <p>{text}</p>
       <div className="icons">
-        <button className="liked" onClick={onToggle}>
-          {liked ? (
-            <img src="https://assets.ccbp.in/frontend/react-js/comments-app/liked-img.png" />
-          ) : (
-            <img src="https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png" />
-          )}
-        </button>
-        <button className="delete-button" onClick={onDeleteComment}>
-          <img
-            className="delete"
-            src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png"
-          />
-        </button>
+        <div className="butons-Container">
+          <button className="liked" onClick={onToggle}>
+            {liked ? (
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/comments-app/liked-img.png"
+                alt="Liked"
+              />
+            ) : (
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png"
+                alt="like"
+              />
+            )}{' '}
+            Like
+          </button>
+        </div>
+        <div className="detele-container">
+          <button
+            className="delete-button"
+            testid="delete"
+            onClick={onDeleteComment}
+          >
+            <img
+              className="delete"
+              alt="delete"
+              src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png"
+            />
+          </button>
+        </div>
       </div>
       <span className="line">
         <hr />
       </span>
-    </li>
+    </ul>
   )
 }
 
